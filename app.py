9a139d7bb51ae9ea29e2746f46fefa08b1d62185
overburn <overburn12@gmail.com>
2023-08-20 14:29:56 -0400

    basic

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import openai
+
+openai_api_key = 'YOUR-OPENAI-API-KEY'
+
+openai.api_key = openai_api_key
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.form['user_message']
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=user_message,
+        max_tokens=50
+    )
+    bot_message = response.choices[0].text
+    return bot_message
+
+if __name__ == '__main__':
     app.run(debug=True)
