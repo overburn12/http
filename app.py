0869eb9589f2b9fa6b4ff3d59952ac18a4af39bd
overburn <overburn12@gmail.com>
2023-09-02 23:39:55 -0400

    favicon.ico stuff

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import openai
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from openai.error import OpenAIError
 
 app = Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 load_dotenv() 
 openai.api_key = os.getenv("MY_API_KEY")
@@ -24,6 +24,10 @@ def process_message(chat_history):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/chat', methods=['POST'])
 def chat():
