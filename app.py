9671dc8670f4925f9610e11f95760cb5929aa0aa
overburn <overburn12@gmail.com>
2023-09-05 19:03:33 -0400

    upgraded flask logger to display external ip address when using  nginx

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import openai
 import os
+import logging
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from openai.error import OpenAIError
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+# Custom log format
+log_format = (
+    '%(asctime)s [%(levelname)s] - ' 
+    'Client IP: %(client_ip)s - ' 
+    '%(message)s'
+)
 
 load_dotenv() 
 openai.api_key = os.getenv("MY_API_KEY")
@@ -21,8 +28,40 @@ def process_message(chat_history):
     except OpenAIError as e:
         return {"error": str(e)}
 
+# Custom log format
+log_format = (
+    '%(asctime)s [%(levelname)s] - ' 
+    'Client IP: %(client_ip)s - ' 
+    '%(message)s'
+)
+
+with app.app_context():
+    # Remove the default handler
+    for handler in app.logger.handlers:
+        app.logger.removeHandler(handler)
+
+    # Create a new handler with the custom format
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(log_format)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+# Update the logger to include client_ip
+@app.before_request
+def before_request():
+    client_ip = request.headers.get('X-Real-IP', request.remote_addr)
+    request._client_ip = client_ip  # Attach IP to the request object
+
+# Inject client_ip into the logger's extra parameter
+def log_request_info(response):
+    app.logger.info('Request Info - %s %s', request.method, request.url, extra={'client_ip': request._client_ip})
+    return response
+
+app.after_request(log_request_info)
+
 @app.route('/')
 def index():
+    #client_ip = request.headers.get('X-Real-IP', request.remote_addr)
     return render_template('index.html')
 
 @app.route('/favicon.ico')
