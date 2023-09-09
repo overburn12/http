fb8f7256360dd050efa2f0f3229bae6220d86142
overburn <overburn12@gmail.com>
2023-09-09 16:19:40 -0400

    remote updater

--- updater.py
+++ updater.py
@@ -7,15 +7,12 @@ commands = [
 
 with open('update.log', 'a') as logfile:
     for command in commands:
-        process = subprocess.Popen(
+        logfile.write(f'Command: {command}\n')
+        result = subprocess.run(
             command,
             shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            stdout=logfile,
+            stderr=logfile,
             text=True
         )
-        stdout, stderr = process.communicate()
-        logfile.write(f'Command: {command}\n')
-        logfile.write(f'Stdout: {stdout}\n')
-        logfile.write(f'Stderr: {stderr}\n')
-        logfile.write('\n')
+        logfile.write(f'Return Code: {result.returncode}\n\n')
