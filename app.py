77b5a7e1d8dcdaa86b642384b205617e46d8693e
overburn <overburn12@gmail.com>
2023-09-09 17:10:21 -0400

    removed cutom logger stuff

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import subprocess, openai, json, os, logging
+import subprocess, openai, json, os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from openai.error import OpenAIError
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-
-# Custom log format
-log_format = '[%(levelname)s] - Client IP: %(client_ip)s - Request Info - %s %s'
 
 load_dotenv() 
 openai.api_key = os.getenv("MY_API_KEY")
@@ -37,29 +33,7 @@ def load_ip_counts():
 
 ip_counts = load_ip_counts()
 
-# Custom log format
-log_format = (
-    '%(asctime)s [%(levelname)s] - ' 
-    'Client IP: %(client_ip)s - ' 
-    '%(message)s'
-)
-
-with app.app_context():
-    # Remove the default handler
-    for handler in app.logger.handlers:
-        app.logger.removeHandler(handler)
-
-    # Create a new handler with the custom format
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(log_format)
-    handler.setFormatter(formatter)
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.INFO)  # Ensure our logger captures INFO level logs
-    # Optionally, if you want to suppress the default Werkzeug logs:
-    logging.getLogger('werkzeug').setLevel(logging.ERROR) 
-
-# Update the logger to include client_ip
-@app.before_request
+@app.before_request        
 def count_misc_requests():
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     
@@ -68,19 +42,6 @@ def count_misc_requests():
 
     if request.endpoint not in ["chat", "index", "count_connections", "view_count_page", "favicon", "static"]:  # Add other known endpoints if needed
         ip_counts[ip]["misc"] += 1
-
-@app.before_request        
-def before_request():
-    client_ip = request.headers.get('X-Real-IP', request.remote_addr)
-    request._client_ip = client_ip  # Attach IP to the request object
-
-
-# Inject client_ip into the logger's extra parameter
-@app.after_request
-def log_request_info(response):
-    app.logger.info('Request Info - %s %s', request.method, request.url, extra={'client_ip': request._client_ip})
-    return response
-
 
 @app.route('/')
 def index():
@@ -111,7 +72,7 @@ def chat():
 
     user_message = request.json['user_message']
     bot_message = process_message(user_message)
-    return jsonify({ 'bot_message': bot_message })
+    return jsonify({'bot_message': bot_message})
 
 @app.route('/count', methods=['GET'])
 def count_connections():
@@ -137,4 +98,4 @@ def update_server():
     return render_template('update.html', log_content=log_content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080) 
+    app.run(host='0.0.0.0', port=8080)
