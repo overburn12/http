cdb8d5e166ae02fe68f408fd9504c06b1a2a9a4e
overburn <overburn12@gmail.com>
2023-09-09 17:31:06 -0400

    updater some more

--- updater.py
+++ updater.py
@@ -1,20 +1,16 @@
 import subprocess
 
-commands = [
-    'git pull',
-    'sudo systemctl restart flask.service'
-]
-
 with open('update.log', 'a') as logfile:
-    for command in commands:
-        logfile.write(f'Command: {command}\n')
-        result = subprocess.run(
-            command,
-            shell=True,
-            capture_output=True,  # Capture the command output
-            text=True
-        )
-        # Write the command output to the log file
-        logfile.write(result.stdout)
-        logfile.write(result.stderr)
-        logfile.write(f'Return Code: {result.returncode}\n\n')
+    logfile.write(f'Command: git pull\n')
+    result = subprocess.run(
+        'git pull',
+        shell=True,
+        capture_output=True,  # Capture the command output
+        text=True
+    )
+    # Write the command output to the log file
+    logfile.write(result.stdout)
+    logfile.write(result.stderr)
+    logfile.write(f'Return Code: {result.returncode}\n\n')
+
+subprocess.run('sudo systemctl restart flask.service', shell=True,text=True)
