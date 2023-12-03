8aed43f0e25a26c76c732978873c48e2be2d9c08
overburn <overburn12@gmail.com>
2023-12-02 22:40:26 -0500

    updated models

--- main.py
+++ main.py
@@ -23,14 +23,16 @@ ollama_models = [
         'orca-mini',
         'wizardlm-uncensored']
 openai_models = [
-        'gpt-3.5-turbo-16k',
         'gpt-3.5-turbo',
         'gpt-3.5-turbo-0301',
         'gpt-3.5-turbo-0613',
+        'gpt-3.5-turbo-1106',
+        'gpt-3.5-turbo-16k',
         'gpt-3.5-turbo-16k-0613',
         'gpt-4',
         'gpt-4-0314',
-        'gpt-4-0613']
+        'gpt-4-0613',
+        'gpt-4-1106-preview']
 default_model = 'gpt-3.5-turbo-16k'
 images = {}
 app_start_time = int(datetime.utcnow().timestamp())
